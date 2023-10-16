5e5b2874b48cf8527422eeb91ff48ddeb98ed5d4
Fabian <fabian.van.treek@gmx.de>
2023-10-16 18:18:44 +0200

    Add navigation excamples

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
-import React, {useState} from "react";
+import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
-import { fetchWeather } from './api/fetchWeather';
-import './App.css';
+import Home from "./home.js";
+import About from './testsite.js';
+import WeatherApp from "./weatherApp.js";
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-
-    const search = async(e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
-
-            setWeather(data);
-            setQuery('');
-        }
-    }
-
     return (
-        <div className="main-container">
-            <input
-                type="text"
-                className="search"
-                placeholder="Serach ..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyPress={search}
-            />
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
-        </div>
+        <Router>
+            <div>
+                <Link to="/">Home</Link>
+                <p/>
+                <Link to="weatherApp">weatherApp</Link>
+                <p/>
+                <Link to="/about">About</Link>
+            </div>
+
+            <Routes>
+                <Route path="/" exact element={<Home />}/>
+
+                <Route path="/weatherApp" exact element={<WeatherApp />}/>
+
+                <Route path="/about" exact element={<About />}/>
+            </Routes>
+        </Router>
     )
 }
 
