35bff991ee8a76246746ab890a4177dff65ca92b
Fabian <fabian.van.treek@gmx.de>
2023-10-18 23:03:57 +0200

    start dropdown navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import React, {useState} from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./home.js";
 import About from './testsite.js';
 import WeatherApp from "./weatherApp.js";
 
+import portrait from "./img/user.png";
+
 const App = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <Router>
-            <div>
-                <Link to="/">Home</Link>
-                <p/>
-                <Link to="weatherApp">weatherApp</Link>
-                <p/>
-                <Link to="/about">About</Link>
+            <div className="App">
+                <div className="menu-container">
+                    <div className="menu-trigger" onClick={()=>{setOpen(!open)}}>
+                        <img src={portrait}></img>
+                    </div>
+
+                    <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
+                        <h3>TestName<br /><span>Website-Test</span></h3>
+                        <ul>
+                            <li className="dropdownItem">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="dropdownItem">
+                                <Link to="/weatherApp">Weather App</Link>
+                            </li>
+                            <li className="dropdownItem">
+                                <Link to="/about">About</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+
+                    <Route path="/weatherApp" exact element={<WeatherApp />} />
+
+                    <Route path="/about" exact element={<About />} />
+                </Routes>
             </div>
-
-            <Routes>
-                <Route path="/" exact element={<Home />}/>
-
-                <Route path="/weatherApp" exact element={<WeatherApp />}/>
-
-                <Route path="/about" exact element={<About />}/>
-            </Routes>
         </Router>
     )
 }
