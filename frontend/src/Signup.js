fc7a64fb8a6603eefb5398cb99b65800b3630110
Fabian <fabian.van.treek@gmx.de>
2023-10-24 21:03:37 +0200

    Login überarbeitet und Registrieren erstellt

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import "./css/login.css"
+import {Link} from 'react-router-dom';
+
+const containerStyle = {
+    backgroundColor: '#f1f1f1'
+};
 
 const Signup = () => {
-    return(
-        <div>
-            <h1>Signup</h1>
-        </div>
+    return (
+        <form action=''>
+            <div className='container'>
+                <h2>Sign up</h2>
+                <label htmlFor='name'>Name</label>
+                <input type='text' placeholder='Enter Name' name='name' required />
+                
+                <label htmlFor='username'>Username</label>
+                <input type='text' placeholder='Enter Username' name='username' required />
+
+                <label htmlFor='password'>Password</label>
+                <input type='password' placeholder='Enter Password' name='password' required />
+
+                <button type='submit'>Sign up</button>
+            </div>
+
+            <div class="container" style={containerStyle}>
+                <button type="button" class="cancelbtn">Cancel</button>
+                <Link to="/login" class="registerbtn">Login</Link>
+            </div>
+        </form>
     );
 };
 
